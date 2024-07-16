6643bca18f1c8262820ada396c67fbf30f00100c
kw4n <kawanaraujocontato@gmail.com>
2024-07-16 17:17:40 -0300

    feat: leitura e escrita de xlsx

--- basics/helper_xlsx.py
+++ basics/helper_xlsx.py
@@ -2,13 +2,14 @@ from typing import List, Any
 import openpyxl
 
 class HelperXlsx:
+
     def read(self, filename: str) -> List[Any]:
         workbook = openpyxl.load_workbook(filename)
         sheet = workbook.active
         data = []
 
         for row in sheet.iter_rows(values_only=True):
-            data.append(row)
+            data.append(list(row))
 
         return data
 
@@ -21,7 +22,20 @@ class HelperXlsx:
 
         workbook.save(filename)
 
-# Exemplo de uso:
-# helper = HelperXlsx()
-# data = helper.read('example.xlsx')
-# helper.write('output.xlsx', data)
+# Nomes posição das colunas
+CHANNEL = 0
+CHANNEL_CONTACT = 1
+AUTOPILOT = 2
+SENTIMENT = 3
+STARTED = 4
+ENDED = 5
+DURATION = 6
+DURATION_SECONDS = 7
+END_OF_INTERACTION = 8
+NUMBER_OF_ERRORS = 9
+CONVERSATION_MISS = 10
+ESCALATION_TIME = 11
+INTERACTION_ID = 12
+OPCAO_SELECIONADA = 13
+JORNADA_DO_CLIENTE_NO_CHATBOT = 14
+FINALIZACAO_DO_CONTATO = 15
