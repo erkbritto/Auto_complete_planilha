f95c0a6a45e1ec8ad11b835652d8a307600e031d
kw4n <kawanaraujocontato@gmail.com>
2024-07-17 16:23:41 -0300

    refactor: removendo codigo de teste

--- search-ticket-data.py
+++
@@ -1,7 +0,0 @@
-from basics.find_ticket_by_id import find_ticket_by_id
-from basics.get_token import update_token
-from time import sleep
-
-update_token()
-
-print(find_ticket_by_id('069ecd639dc54e68a9927d16e5a673c5'))
