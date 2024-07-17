ab2a3723919fb35855685d0481a9df2e0990a97e
Erick Carvalho <erickbritto060@gmail.com>
2024-07-17 17:58:22 -0300

    comentarios

--- basics/getAnalyzers.py
+++ basics/getAnalyzers.py
@@ -1,7 +1,7 @@
-from typing import List
-from basics.analyzers.analizer1 import Analyzer1
+from typing import List  # Importa o tipo List para anotações de tipo
+from basics.analyzers.analizer1 import Analyzer1  # Importa a classe Analyzer1 do módulo especificado
 
-def getAnalyzers():
-    return[
-        Analyzer1()
+def getAnalyzers() -> List[Analyzer1]:  # Define a função que retorna uma lista de instâncias de Analyzer1
+    return [
+        Analyzer1()  # Cria uma instância de Analyzer1 e a adiciona à lista
     ]
