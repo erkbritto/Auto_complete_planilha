ac18fc8872c93c2ecaefedc5a9c1123b7ea53ddc
kw4n <kawanaraujocontato@gmail.com>
2024-07-17 15:33:30 -0300

    fix interface

--- basics/analyzers/analyzer3.py
+++ basics/analyzers/analyzer3.py
@@ -1,10 +1,10 @@
-from analyzer import ChatbotAnalyzer
 from basics.find_ticket_by_id import Message
 from typing import List, Dict
+from basics.qualification import Qualification
 
-class Analyzer2(ChatbotAnalyzer) :
+class Analyzer2 :
     def isValid(self, messages: List[Message]) -> bool:
         pass
 
-    def getQualifications(self, messages: List[Message]) -> Dict[str, str]:
+    def getQualifications(self, messages: List[Message]) -> Qualification:
         pass
---
+++ basics/qualification.py
@@ -0,0 +1,6 @@
+from typing import TypedDict
+
+class Qualification(TypedDict):
+    selectedOption:str
+    customerJourney:str
+    finalizationOfTheContract:str
