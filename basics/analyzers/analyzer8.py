892b9c32bd11dd5dbf732b61e932da1309ef12ea
kw4n <kawanaraujocontato@gmail.com>
2024-07-18 11:37:59 -0300

    feat: Analyzer8

--- basics/analyzers/analyzer8.py
+++ basics/analyzers/analyzer8.py
@@ -2,13 +2,33 @@ from basics.find_ticket_by_id import Message
 from typing import List
 from basics.qualification import Qualification
 
+# d23c095bd54f4cf2bdcc16fa50c78edc
 class Analyzer8 :
     def isValid(self, messages: List[Message]) -> bool:
-        return False
+
+        if not (len(messages) == 10):
+            return False
+        
+        if not (messages[1]["sender"] == 'bot' and messages[0]["message"].startswith("Seja bem-vindo(a) \u00e0 Nio Digital!")):
+            return False
+        
+        if not (messages[3]["sender"] == 'bot' and messages[0]["message"].startswith("\u00d3timo. Em que posso ajudar?")):
+            return False
+        
+        if not (messages[5]["sender"] == 'bot' and messages[0]["message"].startswith("Por favor informe os 4 \u00faltimos d\u00edgitos")):
+            return False
+        
+        if not (messages[7]["sender"] == 'bot' and messages[0]["message"].startswith("O \u00faltimos 4 d\u00edgitos que voc\u00ea digitou est\u00e1 inv\u00e1lido")):
+            return False
+        
+        if not (messages[9]["sender"] == 'bot' and messages[0]["message"].startswith("Tudo bem!\nNio Digital agradece e at\u00e9 logo!")):
+            return False
+
+        return True
 
     def getQualifications(self, messages: List[Message]) -> Qualification:
         return {
-            "selectedOption": '',
-            "customerJourney": '',
-            "finalizationOfTheContract": '',
+            "selectedOption": 'Opção 1',
+            "customerJourney": 'Selecionou 2 opções do menu',
+            "finalizationOfTheContract": 'Contato finalizado por falta de dados corretos',
         }
