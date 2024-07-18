6bedd29242f23b0dbe7b9bf7a0f8cb49b12642d6
kw4n <kawanaraujocontato@gmail.com>
2024-07-18 10:46:34 -0300

    refactor: removendo constantes desnecessárias

--- basics/analyzers/analyzer2.py
+++ basics/analyzers/analyzer2.py
@@ -3,8 +3,6 @@ from typing import List
 from basics.qualification import Qualification
 
 # a3436e09b33c4029838d7062445bf2bc
-MESSAGE_WELCOME = "Seja bem-vindo(a) à Nio Digital!\n\nSou o Alê, seu assistente virtual.\n\nPara obter informações sobre os assuntos abaixo: \n\n- Adquirir o Cartão Nio\n- Consultar o status da sua proposta\n- Informações sobre Saque Parcelado\n- Atendimento para correspondentes Nio Digital\n- Órgãos/Empresas Conveniadas\nClique neste link:  https://api.whatsapp.com/send?phone=551130793682&text=Ol%C3%A1,%20NIO%20Digital\n\nAgora, se você já é cliente NIO Digital e deseja informações sobre o seu Cartão de Crédito Consignado, nosso horário de atendimento com especialistas é de segunda a sexta-feira, das 8h às 20h, exceto em feriados.\n\nPor favor, anote seu protocolo de atendimento: 14062024a3436e09b3. ㅤ\nPara iniciarmos seu atendimento, por favor informe o número do seu CPF. (apenas números)."
-MESSAGE__CAN_I_HELP_YOU = "Ótimo. Em que posso ajudar?\nㅤ *Digite a opção desejada para*:\n\n1 - Limite disponível para utilização\n2 - Informações sobre a sua fatura\n3 - Bloqueio, desbloqueio, 2ª via ou outras informações\n4 - Não reconhecimento de compras ou saques\n5 - Contestação de taxas\n6 - Elogio, sugestão ou reclamação\n7 - Atualização cadastral\n8 - Informações sobre entrega do cartão\n9 - Aplicativo Nio Digital\n10 - Problemas no Boleto/Pagamento da Fatura\n0 - Outros assuntos"
 
 class Analyzer2 :
 
