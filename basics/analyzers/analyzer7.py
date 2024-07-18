6f991a48f51a14bb4d74e56061b1bac711d98112
kw4n <kawanaraujocontato@gmail.com>
2024-07-18 11:17:34 -0300

    feat: Analyzer7

--- basics/analyzers/analyzer7.py
+++ basics/analyzers/analyzer7.py
@@ -2,13 +2,20 @@ from basics.find_ticket_by_id import Message
 from typing import List
 from basics.qualification import Qualification
 
+# 58d2a34bc2154443b17825bb4c653bc9
 class Analyzer7 :
     def isValid(self, messages: List[Message]) -> bool:
-        return False
+        if not (len(messages) == 2):
+            return False
+        
+        if not (messages[1]["sender"] == 'bot' and messages[1]["message"].startswith("Desculpe n\u00e3o entendi. Retornaremos")):
+            return False
+        
+        return True
 
     def getQualifications(self, messages: List[Message]) -> Qualification:
         return {
-            "selectedOption": '',
-            "customerJourney": '',
-            "finalizationOfTheContract": '',
+            "selectedOption": 'Sem opção selecionada',
+            "customerJourney": 'Sem opções selecionadas',
+            "finalizationOfTheContract": 'Sem interação - Sem resposta do cliente',
         }
