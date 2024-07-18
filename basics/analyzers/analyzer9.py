d2c9070b3ff69710de949af24edfa93cfcdb127d
Erick Carvalho <erickbritto060@gmail.com>
2024-07-18 15:13:45 -0300

    analyzer9 99a0a6113bc444df9f63114a500958ef

--- basics/analyzers/analyzer9.py
+++ basics/analyzers/analyzer9.py
@@ -1,40 +1,23 @@
 from basics.find_ticket_by_id import Message
 from typing import List
 from basics.qualification import Qualification
-    #592d0a9c01694c1dbe45df06fba1c863
+#99a0a6113bc444df9f63114a500958ef
 class Analyzer9:
     def isValid(self, messages: List[Message]) -> bool:
-
-        if not (len(messages) == 10):
+        if not (len(messages) == 4):
             return False
         
-        if not (messages[1]["sender"] == 'bot' and messages[0]["message"].startswith("Seja bem-vindo(a) \u00e0 Nio Digital!")):
+        if not (messages[1]["sender"] == 'bot' and "Desculpe n\u00e3o entendi. Retornaremos para o in\u00edcio." in messages[1]["message"]):
             return False
         
-        if not (messages[2]["sender"] == 'bot' and messages[0]["message"].startswith("N\u00e3o conseguimos localizar o CPF informado")):
-            return False
-        
-        if not (messages[0]["sender"] == 'bot' and messages[0]["message"].startswith("\u00d3timo. Em que posso ajudar?")):
-            return False
-        
-        if not (messages[7]["sender"] == 'bot' and messages[0]["message"].startswith("Por favor informe os 4 \u00faltimos d\u00edgitos do cart\u00e3o que deseja consultar.")):
-            return False
-        
-        if not (messages[9]["sender"] == 'bot' and messages[0]["message"].startswith("Tudo bem. Digite a op\u00e7\u00e3o desejada para:")):
-            return False
-        
-        if not (messages[0]["sender"] == 'bot' and messages[0]["message"].startswith("Digite a op\u00e7\u00e3o desejada para:")):
-            return False
-        if not (messages[9]["sender"] == 'bot' and messages[0]["message"].startswith("Digite:\n\n1 - Dificuldade em efetuar compras com o cart\u00e3o.\n2 - Outros assuntos.\n3 - Retornar ao menu anterior.")):
-            return False
-        if not (messages[2]["sender"] == 'bot' and messages[0]["message"].startswith("S\u00f3 um momento enquanto te transferimos para um de nossos agentes.")):
+        if not (messages[3]["sender"] == 'bot' and "N\u00e3o conseguimos localizar o CPF informado, digite-o novamente, mas apenas os n\u00fameros, ok?" in messages[3]["message"]):
             return False
         
         return True
 
     def getQualifications(self, messages: List[Message]) -> Qualification:
         return {
-            "selectedOption": '',
-            "customerJourney": '',
-            "finalizationOfTheContract": '',
+            "selectedOption": 'Sem opção selecionada',
+            "customerJourney": 'Sem opção selecionada',
+            "finalizationOfTheContract": 'Sem interação- sem resposta do cliente',
         }
--- get_json_example.py
+++ get_json_example.py
@@ -1,28 +1,3 @@
-from basics.analyzers.analyzer3 import Analyzer3
-# from basics.find_ticket_by_id import find_ticket_by_id
+from basics.find_ticket_by_id import find_ticket_by_id
 
-
-
-messages = [
-    {
-        "sender": "client",
-        "message": "oi"
-    },
-    {
-        "sender": "bot",
-        "message": "Seja bem-vindo(a) à Nio Digital!\n\nSou o Alê, seu assistente virtual.\n\nPara obter informações sobre os assuntos abaixo: \n\n- Adquirir o Cartão Nio\n- Consultar o status da sua proposta\n- Informações sobre Saque Parcelado\n- Atendimento para correspondentes Nio Digital\n- Órgãos/Empresas Conveniadas\nClique neste link:  https://api.whatsapp.com/send?phone=551130793682&text=Ol%C3%A1,%20NIO%20Digital\n\nAgora, se você já é cliente NIO Digital e deseja informações sobre o seu Cartão de Crédito Consignado, nosso horário de atendimento com especialistas é de segunda a sexta-feira, das 8h às 20h, exceto em feriados.\n\nPor favor, anote seu protocolo de atendimento: 1106202437d0d050c0. ㅤ\nPara iniciarmos seu atendimento, por favor informe o número do seu CPF. (apenas números)."
-    }
-]
-
-
-analyzer = Analyzer3()
-
-isValid = analyzer.isValid(messages)
-
-qualification = analyzer.getQualifications(messages)
-
-print(isValid, qualification)
-
-
-
-# print(find_ticket_by_id('37d0d050c0b24194a85dbdaa883e156d'))
+print(find_ticket_by_id('592d0a9c01694c1dbe45df06fba1c863'))
