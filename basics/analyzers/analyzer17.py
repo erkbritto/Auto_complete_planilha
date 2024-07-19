018abcd905ef1de2e1f91890d606e06525b31479
Erick Carvalho <erickbritto060@gmail.com>
2024-07-19 15:47:40 -0300

    analyzer17

--- basics/analyzers/analyzer17.py
+++ basics/analyzers/analyzer17.py
@@ -4,24 +4,31 @@ from basics.qualification import Qualification
 
 # d935804cff1640dcb6a1393dffaa4fce
 class Analyzer17:
+
     def isValid(self, messages: List[Message]) -> bool:
         if not (len(messages) == 10):
-            return False        
-        if not (messages[1]["sender"] == 'bot' and "Seja bem-vindo(a) à Nio Digital!" in messages[1]["message"]):
-            return False
-        if not (messages[3]["sender"] == 'bot' and "Digite a opção desejada para:" in messages[3]["message"]):
-            return False
-        if not (messages[5]["sender"] == 'bot' and "Por favor informe os 4 últimos dígitos do cartão que deseja consultar." in messages[5]["message"]):
-            return False
-        if not (messages[7]["sender"] == 'bot' and "Seguem as informações solicitadas:" in messages[7]["message"]):
             return False
         
+        if not (messages[1]["sender"] == 'bot' and messages[1]["message"].startswith("Seja bem-vindo")):
+            return False
+        
+        if not (messages[3]["sender"] == 'bot' and messages[3]["message"].startswith("\u00d3timo.")):
+            return False
+        
+        if not (messages[5]["sender"] == 'bot' and messages[5]["message"].startswith("Por favor informe")):
+            return False
+        
+        if not (messages[7]["sender"] == 'bot' and messages[7]["message"].startswith("Seguem as informa\u00e7\u00f5es")):
+            return False
+
+        if not (messages[9]["sender"] == 'bot' and messages[9]["message"].startswith("Ol\u00e1 Seja bem-vindo")):
+            return False
+
         return True
 
     def getQualifications(self, messages: List[Message]) -> Qualification:
-        # Aqui você pode implementar a lógica para extrair qualificações com base nas mensagens
         return {
-            "selectedOption": 'Opção 1',  # Exemplo: Aqui estou assumindo que a opção selecionada foi enviada pelo cliente
+            "selectedOption": 'Opção 1',  
             "customerJourney": 'Selecionou apenas 1 opção',
             "finalizationOfTheContract": 'Sem interação - Sem resposta do cliente',
         }
